ef8d6d64bd833c192d6464775e14cfb5b4e6c187
Raziel Sotken <zromik@mail.ru>
2024-08-22 15:56:21 +0400

    Реализован пакет для проверки конфигурации.

--- setup/__init__.py
+++ setup/__init__.py
@@ -1,5 +1,8 @@
 from .log_setup import configure_logging
+from .config_setup import read_param, check_config_file
 
 __all__ = [
-    'configure_logging'
+    'configure_logging',
+    'read_param',
+    'check_config_file'
 ]
