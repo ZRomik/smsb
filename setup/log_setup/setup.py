0f59293887217cc354ad5d1b3e12a746c9e3a333
ZRomik <zromik@mail.ru>
2024-08-30 21:14:59 +0400

    Для отладочных и информационных сообщений журнала добавлен краткий формат.

--- setup/log_setup/setup.py
+++ setup/log_setup/setup.py
@@ -7,15 +7,17 @@ def setup_logging(level=logging.DEBUG):
     # отключение отладочных сообщений библиотек peewee и aiogram
     logging.getLogger("peewee").setLevel(logging.ERROR)
     logging.getLogger("aiogram").setLevel(logging.ERROR)
-    msg_fmt = "[%(asctime)-19s:%(msecs)-03d] %(levelname)-8s {%(filename)s %(funcName)s:%(lineno)d} > %(message)-s"
+    detailed_fmt = "[%(asctime)-19s.%(msecs)-03d] %(levelname)-8s {%(module)-s.%(funcName)-s:%(lineno)-02d (%(pathname)-s)} : %(message)s"
+    brief_fmt = "[%(asctime)-19s.%(msecs)-03d] %(levelname)-8s : %(message)s"
     date_fmt = "%d.%m.%Y в %H:%M:%S"
-    formatter = logging.Formatter(fmt=msg_fmt, datefmt=date_fmt)
+    detailed_formatter = logging.Formatter(fmt=detailed_fmt, datefmt=date_fmt)
+    brief_formatter = logging.Formatter(fmt=brief_fmt, datefmt=date_fmt)
 
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
+    console_handler.setFormatter(brief_formatter)
     console_handler.setLevel(logging.DEBUG)
     file_handler = logging.FileHandler("errors.log", encoding='utf-8', mode='w')
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(detailed_formatter)
     file_handler.setLevel(logging.WARNING)
 
     logging.basicConfig(
