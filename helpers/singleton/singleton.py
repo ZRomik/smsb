bfc894745929de282f44c3a2480078d843b2b9f9
Raziel Sotken <zromik@mail.ru>
2024-09-03 10:40:43 +0400

    Добавлено сообщение об инициализации касса.

--- helpers/singleton/singleton.py
+++ helpers/singleton/singleton.py
@@ -1,7 +1,13 @@
+import logging
 class Singleton:
     """
     Реализация паттерна Одиночка
     """
+    def __init__(self):
+        super().__init__()
+        logging.debug(
+            "Инициализация %s" % self.__class__.__name__
+        )
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
             cls._instance = super().__new__(cls, *args, **kwargs)
